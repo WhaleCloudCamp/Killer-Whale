940c61aa44281c089e50f26c199fe29042e6da37
xiaohuoni <448627663@qq.com>
2018-10-25 14:15:32 +0800

    修复whale-rn没有编译成es5，读取错误的bug

--- .umirc.js
+++ .umirc.js
@@ -1,26 +1,30 @@
-const path = require('path')
+const path = require("path");
 
 export default {
   plugins: [
-    ['umi-plugin-react',{
-      dva:true,
-      antd:true,
-      routes: {
-        exclude: [
-          /model\.(j|t)sx?$/,
-          /service\.(j|t)sx?$/,
-          /models\//,
-          /components\//,
-          /services\//,
-        ],
-      },
-    }]
+    [
+      "umi-plugin-react",
+      {
+        dva: true,
+        antd: true,
+        routes: {
+          exclude: [
+            /model\.(j|t)sx?$/,
+            /service\.(j|t)sx?$/,
+            /models\//,
+            /components\//,
+            /services\//
+          ]
+        }
+      }
+    ]
   ],
   alias: {
     "react-native$": "react-native-web",
-    components:path.resolve(__dirname,'src/components'),
-    utils:path.resolve(__dirname,'src/utils'),
-    services:path.resolve(__dirname,'src/services'),
-    models:path.resolve(__dirname,'src/models'),
+    components: path.resolve(__dirname, "src/components"),
+    utils: path.resolve(__dirname, "src/utils"),
+    services: path.resolve(__dirname, "src/services"),
+    models: path.resolve(__dirname, "src/models")
   },
+  extraBabelIncludes:[/whale-rn/]
 };
