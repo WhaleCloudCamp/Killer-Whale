035ffcdc2c5e3427ef7856fea1f91c5e34359560
lwq321 <825780118@qq.com>
2018-11-02 17:26:24 +0800

    配置表修改

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,6 +1,28 @@
-import {Slider} from 'antd-mobile';
+import { Slider } from 'antd-mobile';
 import 'antd-mobile/lib/slider/style';
-export default Slider;
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class Sliders extends React.Component {
+
+    render() {
+        return (
+            <View style={{
+                borderWidth: 15, borderColor: 'white'
+             }}>
+                <Slider
+                    min={this.props.min}
+                    max={this.props.max}
+                    step={this.props.step}
+                    value={this.props.value}
+                    disabled={this.props.disabled}
+                    maximumTrackTintColor={this.props.maximumTrackTintColor}
+                ></Slider>
+
+            </View>
+        )
+    }
+}
 
 
 
