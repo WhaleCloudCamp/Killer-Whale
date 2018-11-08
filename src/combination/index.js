dbcfa9711ed7e830950b366742947dd222ce9fa1
lwq321 <825780118@qq.com>
2018-11-08 18:21:40 +0800

    删除TabBadge,修改代码

--- src/combination/index.js
+++ src/combination/index.js
@@ -1,5 +1,4 @@
 export { default as SwitchItem } from "../combination/SwitchItem/index";
-export { default as TabBadge } from "../combination/TabBadge/index";
 export { default as CheckboxItem } from "../combination/CheckItem/index";
 export { default as RadioItem } from "../combination/RadioItem/index";
 export { default as StepperItem } from "../combination/StepperItem/index";
@@ -8,9 +7,7 @@ export { default as NavBar } from "../combination/NavBar/index";
 export { default as Slider } from "../combination/Slider/index";
 export { default as Carousel } from "../combination/Carousel/index";
 export { default as Tag } from "../combination/Tag/index";
-export {
-  default as ActivityIndicator
-} from "../combination/ActivityIndicator/index";
+export { default as ActivityIndicator} from "../combination/ActivityIndicator/index";
 export { default as List } from "../combination/List/index";
 export { default as NoticeBar } from "../combination/NoticeBar/index";
 export { default as DatePicker } from "../combination/DatePicker/index";
